37e718a1f7881a78c9b1a420d24525014341a34b
Antonyan7 <antonyansergey2002@gmail.com>
2021-02-19 05:10:46 +0400

    FEAT: handle update by id functionality

--- getImageKeywords.js
+++ getImageKeywords.js
@@ -8,19 +8,33 @@ let count = 0;
 
 const imagesApi = new sstk.ImagesApi()
 
+const findKeywords = async (data) => {
+    return new Promise(resolve => {
+
+        const update = {$set: {keywords: data.keywords}};
+        Photo.findByIdAndUpdate(data.id, update, {overwrite: true}, (err, res) => {
+            console.log(res);
+            console.log(err,222);
+            resolve(res);
+        });
+    })
+}
+
 const getKeywordsAndUpdatePhoto = async () => {
 
     const images = await Photo.find().skip(count).limit(20).exec();
+
     const data = images.map(image => {
         return {
-            id: image.photo_id,
+            id: image.id,
+            photo_id: image.photo_id,
             keywords: image.keywords
         }
     })
 
-    const ids = data.filter(function(item) {
-        return item.id;
-    }).map(function(item) { return item.id; });
+    const photoIds = data.filter(function(item) {
+        return item.photo_id;
+    }).map(function(item) { return item.photo_id; });
 
     const queryParams = {
         "view": "full",
@@ -29,27 +43,36 @@ const getKeywordsAndUpdatePhoto = async () => {
 
     if (count < 1000000) {
         try {
-            const response = await imagesApi.getImageList(ids, queryParams);
+            const response = await imagesApi.getImageList(photoIds, queryParams);
             const imagesKeywords = response.data.map(image => {
                 return {
-                    id: image.id,
+                    id: images.find(photo => photo.photo_id === image.id).id,
+                    photo_id: image.id,
                     keywords: image.keywords
                 }
             });
+            imagesKeywords.sort(function(a, b) {
+                return photoIds.indexOf(a.photo_id) - photoIds.indexOf(b.photo_id);
+            });
 
-            imagesKeywords.map(async (data, index) => {
-                // if (data[index] && data[index].keywords.length === 0) {
-                    const update = {$set: {keywords: data.keywords}};
-                    await Photo.updateOne({photo_id: data.id}, update, {overwrite: true}, (err, res) => {
-                        console.log(res);
-                    });
-                // } else {
-                //     console.log("already Keywords exists")
-                // }
-            })
+            // imagesKeywords.map(async (data, index) => {
+            //     // if (data[index] && data[index].keywords.length === 0) {
+            //         const update = {$set: {keywords: data.keywords}};
+            //         Photo.updateOne({photo_id: data.id}, update, {overwrite: true}, (err, res) => {
+            //             console.log(res);
+            //         });
+            //     // } else {
+            //     //     console.log("already Keywords exists")
+            //     // }
+            // })
+            for(let i = 0; i < imagesKeywords.length; i++){
+                await findKeywords(imagesKeywords[i]);
+            }
+
             count += 20;
             return true;
         } catch (e) {
+            console.log(e);
             console.log("catched");
             return false;
         }
@@ -58,6 +81,7 @@ const getKeywordsAndUpdatePhoto = async () => {
 
 async function recursiveKeywordMining() {
     const response = await getKeywordsAndUpdatePhoto();
+
     if (response && count < 1000000) {
         await recursiveKeywordMining()
     }
